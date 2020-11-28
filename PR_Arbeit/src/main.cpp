2a6908798785e0600d95368f730624798988ebca
alexandergrill <grillalexander0@gmail.com>
2020-11-28 15:08:16 +0100

    add folder/workfiles for pra

---
+++ PR_Arbeit/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * author:  Alexander Grill
+ * file:    main.cpp
+ * project: santa_claus_problem
+ * date:    28.11.2020
+*/
+
+#include "include/CLI11.hpp"
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+
+}
