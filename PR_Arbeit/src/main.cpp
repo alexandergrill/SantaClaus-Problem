50aedd33374b144c64b1f5be486c296120b8a69d
alexandergrill <grillalexander0@gmail.com>
2020-12-30 19:35:15 +0100

    add CLI11 and range bibl.

--- PR_Arbeit/src/main.cpp
+++ PR_Arbeit/src/main.cpp
@@ -5,7 +5,8 @@
  * date:    28.11.2020
 */
 
-#include "include/CLI11.hpp"
+#include "CLI11.hpp"
+#include "rang.hpp"
 #include <iostream>
 #include <thread>
 #include <condition_variable>
@@ -14,16 +15,16 @@
 #include <chrono>
 
 using namespace std;
+using namespace rang;
 
 std::condition_variable santaSem;
 std::condition_variable reindeerSem;
 std::mutex mx;
-
-
+int reindeer{0};
 
 class SantaClaus{
 private:
-    int reindeer{0};
+
 
 public:
     SantaClaus(){
@@ -34,12 +35,12 @@ public:
             unique_lock<mutex> ul{mx};
             reindeerSem.wait(ul, [] { return (reindeer != 9) ? true : false; });
             if(reindeer < 9 ){
-                cout << "The reindeers are not enough to fly only " << reindeer << "\n" << flush;
+                cout << fg::red << "The reindeers are not enough to fly only " << reindeer << "\n" << flush;
             }
             if(reindeer == 9){
                 cout << "Start\n" << flush;
             }
-            this_thread::sleep_for(2s);
+            this_thread::sleep_for(3s);
         }
     }
     void addReindeer(){
@@ -63,15 +64,15 @@ public:
         while(true){
             random_device rd;
             mt19937 gen{rd()};
-            uniform_real_distribution<> dis{1, 5};
+            uniform_real_distribution<> dis{1, 10};
             double time = dis(gen);
             int t = time * 1000;
             this_thread::sleep_for(chrono::milliseconds(t));
-            lock_guard<mutex> lg(mx);
-            cout << "Additional Reindeer is ready\n" << flush;
+            //lock_guard<mutex> lg(mx);
+            cout << fg::yellow << "Additional Reindeer is ready\n"<< flush;
             santa->addReindeer();
             if(santa->getReindeer() == 9){
-                cout << "Santa Claus we can drive\n" << flush;
+                cout << fg::green << "Santa Claus we can fly\n" << flush;
                 reindeerSem.notify_one();
                 break;
             }
@@ -81,6 +82,18 @@ public:
 };
 
 int main(int argc, char *argv[]){
+    
+    int reendiernum{9};
+    int elvesnum{3};
+    int time{24};
+
+    CLI::App app("Santa Claus Problem");
+    app.add_flag("-r,--r", reendiernum, "number of reindeer, which will be needed to fly");
+    app.add_flag("-e,--e", elvesnum, "number of elves, that work in the factory");
+    app.add_flag("-t,--t", time, "number of hours until christmas");
+
+    CLI11_PARSE(app, argc, argv);
+
     SantaClaus sc;
     Reindeers rs(&sc);
 
