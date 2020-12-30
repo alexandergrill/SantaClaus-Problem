c57110bb2d866c856b52bd40e8b786f161ae367f
alexandergrill <grillalexander0@gmail.com>
2020-12-30 14:28:44 +0100

    create SantaClaus, Reindeers Class + Basics

--- PR_Arbeit/src/main.cpp
+++ PR_Arbeit/src/main.cpp
@@ -8,8 +8,36 @@
 #include "include/CLI11.hpp"
 #include <iostream>
 #include <thread>
+#include <condition_variable>
+#include <mutex>
 
 using namespace std;
+
+class SantaClaus{
+private:
+    int elves{0};
+    int reindeer{0};
+    condition_variable santaSem;
+    condition_variable reindeerSem;
+public:
+    SantaClaus();
+    void operator()(){
+
+    }
+};
+
+class Reindeers{
+private:
+    SantaClaus* santa;
+    mutex mx;
+public:
+    Reindeers(SantaClaus* s){
+        santa = s;
+    }
+    void operator()(){
+
+    }
+};
 
 int main(int argc, char *argv[]){
 
